e03be2b53ba95b4751cdfd711eab57aa49afe55c
Fatih <118773961+F4T1H5@users.noreply.github.com>
2025-12-02 19:17:19 +0300

    For Core Project Session is added.

---
+++ CORE/APP/Services/Session/MVC/SessionService.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace CORE.APP.Services.Session.MVC
+{
+    public class SessionService : SessionServiceBase
+    {
+        public SessionService(IHttpContextAccessor httpContextAccessor) : base(httpContextAccessor)
+        {
+        }
+    }
+}
---
+++ CORE/APP/Services/Session/MVC/SessionServiceBase.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Text;
+using System.Text.Json;
+
+namespace CORE.APP.Services.Session.MVC
+{
+    public abstract class SessionServiceBase
+    {
+        protected IHttpContextAccessor HttpContextAccessor { get; }
+
+        protected SessionServiceBase(IHttpContextAccessor httpContextAccessor)
+        {
+            HttpContextAccessor = httpContextAccessor;
+        }
+
+        public virtual T GetSession<T>(string key) where T : class
+        {
+            var value = HttpContextAccessor.HttpContext.Session.GetString(key);
+            if (string.IsNullOrEmpty(value))
+                return null;
+            return JsonSerializer.Deserialize<T>(value);
+        }
+
+        public virtual void SetSession<T>(string key, T instance) where T : class
+        {
+            if (instance is not null)
+            {
+                var value = JsonSerializer.Serialize(instance);
+                HttpContextAccessor.HttpContext.Session.SetString(key, value);
+            }
+        }
+
+        public virtual void RemoveSession(string key)
+        {
+            HttpContextAccessor.HttpContext.Session.Remove(key);
+        }
+    }
+}
